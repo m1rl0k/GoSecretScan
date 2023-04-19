2b0726065ec99586a8c84c3b63f433f253df0de0
M1RL0K <11264689+m1rl0k@users.noreply.github.com>
2023-04-18 22:14:38 -0400

    Update main.go

--- main.go
+++ main.go
@@ -85,106 +85,113 @@ func main() {
 		fmt.Printf("%sSecrets found:%s\n", RedColor, ResetColor)
 		for _, secret := range secretsFound {
 			fmt.Printf("%sFile:%s %s\n%sLine Number:%s %d\n%sLine Number:%s %s\n\n", YellowColor, ResetColor, secret.File, YellowColor, ResetColor, secret.LineNumber, YellowColor, ResetColor, secret.Line)
-}
-fmt.Printf("%s%s\n", YellowColor, SeparatorLine)
-fmt.Printf("%s%d secrets found. Please review and remove them before committing your code.%s\n", RedColor, len(secretsFound), ResetColor)
-os.Exit(1) // Exit with a non-zero exit code, indicating a failure
-} else {
-fmt.Printf("%sNo secrets found.%s\n", GreenColor, ResetColor)
-}
-}
-func shouldIgnore(path string) bool {
-ignorePatterns := []string{
-^\..*, // Hidden files/directories
-^vendor, // Vendor directories
-^node_modules, // Node.js modules directory
-^\.idea, // IntelliJ IDEA project directory
-^\.vscode, // Visual Studio Code project directory
-\.log$, // Log files
-\.out$, // Binary files
-\.min\.js$, // Minified JavaScript files
-\.min\.css$, // Minified CSS files
-\.(jpg|jpeg|png|gif|ico)$, // Images
-\.(pdf|docx?|xlsx?|pptx?)$, // Documents
-}
-for _, pattern := range ignorePatterns {
-re := regexp.MustCompile(pattern)
-if re.MatchString(path) {
-return true
-}
-}
-return false
-}
-func getSecretPatterns() []*regexp.Regexp {
-defaultPatterns := []string{
-(?i)ACCESS_KEY,
-(?i)SECRET_KEY,
-(?i)AUTH_TOKEN,
-(?i)PASSWORD,
-(?i)DB_CONNECTION,
-(?i)API_SECRET,
-}
-patterns := append(defaultPatterns, AdditionalSecretPatterns()...)
-var compiledPatterns []*regexp.Regexp
-for _, pattern := range patterns {
-compiledPattern, err := regexp.Compile(pattern)
-if err != nil {
-fmt.Println("Error compiling pattern:", err)
-os.Exit(1)
-}
-compiledPatterns = append(compiledPatterns, compiledPattern)
-}
-return compiledPatterns
-}
-func scanFileForSecrets(filePath string, secretPatterns []*regexp.Regexp) ([]Secret, error) {
-var secrets []Secret
-file, err := os.Open(filePath)
-if err != nil {
-return secrets, err
-}
-defer file.Close()
-scanner := bufio.NewScanner(file)
-lineNumber := 1
-for scanner.Scan() {
-line := scanner.Text()
-for _, pattern := range secretPatterns {
-if pattern.MatchString(line) {
-secrets = append(secrets, Secret{filePath, lineNumber, line})
-}
-}
-lineNumber++
-}
-if err := scanner.Err(); err != nil {
-return secrets, err
-}
-return secrets, nil
+		}
+		fmt.Printf("%s%s\n", YellowColor, SeparatorLine)
+		fmt.Printf("%s%d secrets found. Please review and remove them before committing your code.%s\n", RedColor, len(secretsFound), ResetColor)
+		os.Exit(1) // Exit with a non-zero exit code, indicating a failure
+	} else {
+		fmt.Printf("%sNo secrets found.%s\n", GreenColor, ResetColor)
+	}
 }
 
+func shouldIgnore(path string) bool {
+    ignorePatterns := []string{
+        `^\..*`,                // Hidden files/directories
+        `^vendor`,              // Vendor directories
+        `^node_modules`,        // Node.js modules directory
+        `^\.idea`,              // IntelliJ IDEA project directory
+        `^\.vscode`,            // Visual Studio Code project directory
+        `\.log$`,               // Log files
+        `\.out$`,               // Binary files
+        `\.min\.js$`,           // Minified JavaScript files
+        `\.min\.css$`,          // Minified CSS files
+        `\.(jpg|jpeg|png|gif|ico)$`,    // Images
+        `\.(pdf|docx?|xlsx?|pptx?)$`,   // Documents
+    }
+
+    for _, pattern := range ignorePatterns {
+        re := regexp.MustCompile(pattern)
+        if re.MatchString(path) {
+            return true
+        }
+    }
+
+    return false
+}
+
+func getSecretPatterns() []*regexp.Regexp {
+	defaultPatterns := []string{
+		`(?i)ACCESS_KEY`,
+		`(?i)SECRET_KEY`,
+		`(?i)AUTH_TOKEN`,
+		`(?i)PASSWORD`,
+		`(?i)DB_CONNECTION`,
+		`(?i)API_SECRET`,
+	}
+	patterns := append(defaultPatterns, AdditionalSecretPatterns()...)
+	var compiledPatterns []*regexp.Regexp
+	for _, pattern := range patterns {
+		compiledPattern, err := regexp.Compile(pattern)
+		if err != nil {
+			fmt.Println("Error compiling pattern:", err)
+			os.Exit(1)
+		}
+		compiledPatterns = append(compiledPatterns, compiledPattern)
+	}
+	return compiledPatterns
+}
+
+func scanFileForSecrets(filePath string, secretPatterns []*regexp.Regexp) ([]Secret, error) {
+	var secrets []Secret
+	file, err := os.Open(filePath)
+	if err != nil {
+		return secrets, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	lineNumber := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		for _, pattern := range secretPatterns {
+			if pattern.MatchString(line) {
+				secrets = append(secrets, Secret{filePath, lineNumber, line})
+			}
+		}
+		lineNumber++
+	}
+	if err := scanner.Err(); err != nil {
+		return secrets, err
+	}
+	return secrets, nil
+}
+
+
 func AdditionalSecretPatterns() []string {
-vulnerabilityPatterns := []string{
-// Add your additional regex patterns here
-(?i)(<\s*script\b[^>]*>(.*?)<\s*/\s*script\s*>), // Cross-site scripting (XSS)
-(?i)(\b(?:or|and)\b\s*[\w-]*\s*=\s*[\w-]*\s*\b(?:or|and)\b\s*[^\s]+), // SQL injection
-(?i)(['"\s]exec(?:ute)?\s*[(\s]*\s*@\w+\s*), // SQL injection (EXEC, EXECUTE)
-(?i)(['"\s]union\s*all\s*select\s*[\w\s,]+(?:from|into|where)\s*\w+), // SQL injection (UNION ALL SELECT)
-(?i)example_pattern_1\s*=\s*"([a-zA-Z0-9\-]+\.example)",
-(?i)example_pattern_2\s*=\s*"([0-9]{12}-[a-zA-Z0-9_]{32})",
-// Private SSH keys
------BEGIN\sRSA\sPRIVATE\sKEY-----[\s\S]+-----END\sRSA\sPRIVATE\sKEY-----,
-// S3 Bucket URLs
-(?i)s3\.amazonaws\.com/[\w\-\.]+,
-// Hardcoded IP addresses
-\b(?:\d{1,3}\.){3}\d{1,3}\b,
-// Hardcoded domains
-(?i)(?:http|https|ftp|sftp)://[^\s'"]+,
-// Basic Authentication credentials
-(?i)(?:http|https)://\w+:\w+@[\w\-\.]+,
-// JWT tokens
-(?i)ey(?:J[a-zA-Z0-9_-]+)[.](?:[a-zA-Z0-9_-]+)[.](?:[a-zA-Z0-9_-]+),
-// Email addresses
-[\w.-]+@[\w.-]+\.\w+,
-// Connection strings (such as database connections)
-(?i)(?:Server|Host)=([\w.-]+);\s*(?:Port|Database|User\sID|Password)=([^;\s]+)(?:;\s(?:Port|Database|User\s*ID|Password)=([^;\s]+))*,
+	vulnerabilityPatterns := []string{
+		// Add your additional regex patterns here
+		`(?i)(<\s*script\b[^>]*>(.*?)<\s*/\s*script\s*>)`, // Cross-site scripting (XSS)
+		`(?i)(\b(?:or|and)\b\s*[\w-]*\s*=\s*[\w-]*\s*\b(?:or|and)\b\s*[^\s]+)`, // SQL injection
+		`(?i)(['"\s]exec(?:ute)?\s*[(\s]*\s*@\w+\s*)`, // SQL injection (EXEC, EXECUTE)
+		`(?i)(['"\s]union\s*all\s*select\s*[\w\s,]+(?:from|into|where)\s*\w+)`, // SQL injection (UNION ALL SELECT)
+		`(?i)example_pattern_1\s*=\s*"([a-zA-Z0-9\-]+\.example)"`,
+		`(?i)example_pattern_2\s*=\s*"([0-9]{12}-[a-zA-Z0-9_]{32})"`,
+		// Private SSH keys
+		`-----BEGIN\sRSA\sPRIVATE\sKEY-----[\s\S]+-----END\sRSA\sPRIVATE\sKEY-----`,
+		// S3 Bucket URLs
+		`(?i)s3\.amazonaws\.com/[\w\-\.]+`,
+		// Hardcoded IP addresses
+		`\b(?:\d{1,3}\.){3}\d{1,3}\b`,
+		// Hardcoded domains
+		`(?i)(?:http|https|ftp|sftp)://[^\s'"]+`,
+		// Basic Authentication credentials
+		`(?i)(?:http|https)://\w+:\w+@[\w\-\.]+`,
+		// JWT tokens
+		`(?i)ey(?:J[a-zA-Z0-9_-]+)[.](?:[a-zA-Z0-9_-]+)[.](?:[a-zA-Z0-9_-]+)`,
+		// Email addresses
+		`[\w.-]+@[\w.-]+\.\w+`,
+		// Connection strings (such as database connections)
+		`(?i)(?:Server|Host)=([\w.-]+);\s*(?:Port|Database|User\sID|Password)=([^;\s]+)(?:;\s(?:Port|Database|User\s*ID|Password)=([^;\s]+))*`,
+	}
+	return vulnerabilityPatterns
 }
-return vulnerabilityPatterns
-}
+
